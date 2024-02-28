6c80ae7543e9b9b9f91c29fe1c4efdceceec512c
Stephen Ng <stephen5.ng@gmail.com>
2024-02-28 11:52:21 -0800

    Adjust pins.

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,25 @@
 #define PANEL_RES_Y 64  // Number of pixels tall of each INDIVIDUAL panel module.
 #define PANEL_CHAIN 1   // Total number of panels chained one to another
 
-MatrixPanel_I2S_DMA *dma_display;
+MatrixPanel_I2S_DMA *hub75_display;
 
 void setup() {
   Serial.begin(112500);
 
   HUB75_I2S_CFG::i2s_pins _pins = {
-    33,  //R1_PIN,
+    25,  //R1_PIN,
     26,  //G1_PIN,
-    25,  //B1_PIN,
-    14,  //R2_PIN,
+    33,  //B1_PIN,
+    13,  //R2_PIN,
     27,  //G2_PIN,
-    13,  //B2_PIN,
-    5,   //A_PIN,
+    14,  //B2_PIN,
+    19,   //A_PIN,
     21,  //B_PIN,
     4,   //C_PIN,
-    17,  //D_PIN,
+    22,  //D_PIN, 
     12,  //E_PIN,
-    2,   //LAT_PIN,
-    15,  //OE_PIN,
+    15,   //LAT_PIN, ** 2 **
+    2,  //OE_PIN, ** 15 ** 
     16,  //CLK_PIN
   };
 
@@ -33,20 +33,20 @@ void setup() {
     PANEL_CHAIN,
     _pins
   );
-
-  dma_display = new MatrixPanel_I2S_DMA(mxconfig);
-  dma_display->begin();
-  dma_display->setBrightness(255);
-  dma_display->setTextSize(8);      // size 1 == 8 pixels high
-  dma_display->setTextWrap(false);  // Don't wrap at end of line - will do ourselves
-  dma_display->clearScreen();
+  mxconfig.clkphase = false;
+  hub75_display = new MatrixPanel_I2S_DMA(mxconfig);
+  hub75_display->begin();
+  hub75_display->setBrightness(255);
+  hub75_display->setTextSize(9);      // size 1 == 8 pixels high
+  hub75_display->setTextWrap(false);  // Don't wrap at end of line - will do ourselves
+  hub75_display->clearScreen();
 }
 
 void loop() {
   static char str[] = "A";
-  dma_display->clearScreen();
-  dma_display->setCursor(12, 4);
-  dma_display->print(str);
+  hub75_display->clearScreen();
+  hub75_display->setCursor(1, 0);
+  hub75_display->print(str);
   Serial.println(str);
   if (str[0]++ == 'Z') {
     str[0] = 'A';
